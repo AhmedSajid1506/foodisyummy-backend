ef4fb71c7124d3090592a6959de8ac3808617e6f
Ahmed Sajid <116376036+AhmedSajid1506@users.noreply.github.com>
2023-01-04 12:51:18 +0500

    Add files via upload

---
+++ db/db.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const mongoose = require('mongoose');
+
+const mongoURI = process.env.MONGO_URL;
+
+const connectToMongo = ()=>{
+  mongoose.connect(mongoURI, ()=>{
+    console.log("Connected to Mongo successfully");
+  })
+}
+
+module.exports = connectToMongo;
